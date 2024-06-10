c62091e64351b215becb482d220b928ea0850e91
jjgroenendijk <3110270+jjgroenendijk@users.noreply.github.com>
2024-06-10 17:29:53 +0200

    Updated documentation

--- payloadCoder/decoder.cpp
+++ payloadCoder/decoder.cpp
@@ -1,81 +1,122 @@
 #include "decoder.h"
 #include <iostream> // cout, endl // debugging only
 
+/// @brief Constructs a new payloadDecoder object.
 payloadDecoder::payloadDecoder() : _buffer{nullptr}
 {
 }
 
+/// @brief Destructor for the payloadDecoder class.
 payloadDecoder::~payloadDecoder()
 {
 }
 
 void payloadDecoder::decodePayload()
 {
-  // print paylaod in binary format
-  std::cout << "Decoder payload binary: ";
-  for (int i = 0; i < _bufferSize; i++)
-  {
-    for (int j = 7; j >= 0; j--)
+    /**
+     * Decodes the payload data.
+     * This function prints the payload in binary format and extracts various data fields from the payload buffer.
+     */
+    std::cout << "Decoder payload binary: ";
+    for (int i = 0; i < _bufferSize; i++)
     {
-      std::cout << ((_buffer[i] >> j) & 1);
+        for (int j = 7; j >= 0; j--)
+        {
+            std::cout << ((_buffer[i] >> j) & 1);
+        }
+        std::cout << " ";
     }
-    std::cout << " ";
-  }
 
-  std::cout << std::endl;
+    std::cout << std::endl;
 
-  _id = static_cast<int>(extract_uint32(_buffer, 0));
-  _version = static_cast<int>(extract_uint8(_buffer, 4));
-  _doorStatus = extract_bool(_buffer, 5, 2);
-  _catchDetect = extract_bool(_buffer, 5, 1);
-  _trapDisplacement = extract_bool(_buffer, 5, 0);
-  _batteryStatus = static_cast<int>(extract_uint8(_buffer, 6));
-  _unixTime = static_cast<int>(extract_uint32(_buffer, 7));
+    _id = static_cast<int>(extract_uint32(_buffer, 0));
+    _version = static_cast<int>(extract_uint8(_buffer, 4));
+    _doorStatus = extract_bool(_buffer, 5, 2);
+    _catchDetect = extract_bool(_buffer, 5, 1);
+    _trapDisplacement = extract_bool(_buffer, 5, 0);
+    _batteryStatus = static_cast<int>(extract_uint8(_buffer, 6));
+    _unixTime = static_cast<int>(extract_uint32(_buffer, 7));
 }
 
-// UINT8
 uint8_t payloadDecoder::extract_uint8(const uint8_t *buf, const unsigned char idx)
 {
-  return static_cast<uint8_t>(buf[idx]);
+    /**
+     * Extracts a uint8_t value from the given buffer at the specified index.
+     *
+     * @param buf The buffer containing the data.
+     * @param idx The index of the value to extract.
+     * @return The extracted uint8_t value.
+     */
+    return static_cast<uint8_t>(buf[idx]);
 }
 
-// UINT16
 uint16_t payloadDecoder::extract_uint16(const uint8_t *buf, const unsigned char idx)
 {
-  uint16_t value{0};
-  value = static_cast<uint16_t>(buf[idx] << 8); // msb
-  value |= static_cast<uint16_t>(buf[idx + 1]);   // lsb
-  return value;
+    /**
+     * @brief Extracts a 16-bit unsigned integer from a buffer at the specified index.
+     *
+     * This function takes a buffer `buf` and an index `idx` and extracts a 16-bit unsigned integer
+     * from the buffer starting at the specified index. The most significant byte is located at `buf[idx]`
+     * and the least significant byte is located at `buf[idx + 1]`.
+     *
+     * @param buf Pointer to the buffer containing the data.
+     * @param idx Index of the most significant byte in the buffer.
+     * @return The extracted 16-bit unsigned integer value.
+     */
+
+    uint16_t value{0};
+    value = static_cast<uint16_t>(buf[idx] << 8); // msb
+    value |= static_cast<uint16_t>(buf[idx + 1]); // lsb
+    return value;
 }
 
-// UINT32
 uint32_t payloadDecoder::extract_uint32(const uint8_t *buf, const unsigned char idx)
 {
-  uint32_t value{0};
-  for (uint8_t i = 0; i < 4; i++)
-  {
-    value |= static_cast<uint32_t>(buf[idx + i] << (24 - (i * 8))); // msb
-  }
-  return value;
+    /**
+     * Extracts a 32-bit unsigned integer from a buffer at the specified index.
+     * This function takes a buffer `buf` and an index `idx` and extracts a 32-bit unsigned integer
+     * from the buffer starting at the specified index. The most significant byte is located at `buf[idx]`
+     * and the least significant byte is located at `buf[idx + 3]`.
+     * @param buf The buffer containing the data.
+     * @param idx The index in the buffer where the 32-bit integer starts.
+     * @return The extracted 32-bit unsigned integer.
+     */
+
+    uint32_t value{0};
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        value |= static_cast<uint32_t>(buf[idx + i] << (24 - (i * 8))); // msb
+    }
+    return value;
 }
 
-// extract boolean
 bool payloadDecoder::extract_bool(const uint8_t *buf, const unsigned char idx, unsigned int pos)
 {
-  bool value;
-  value = (buf[idx] >> pos) & 1;
-  return value;
+    /**
+     * Extracts a boolean value from a buffer at a specified index and position.
+     *
+     * @param buf The buffer containing the data.
+     * @param idx The index of the byte in the buffer.
+     * @param pos The position of the bit within the byte.
+     * @return The extracted boolean value.
+     */
+    bool value;
+    value = (buf[idx] >> pos) & 1;
+    return value;
 }
 
 // print payload decoded
 void payloadDecoder::printPayloadDecoded()
 {
-  std::cout << "Payload decoded: " << std::endl;
-  std::cout << "ID: " << _id << std::endl;
-  std::cout << "Version: " << static_cast<int>(_version) << std::endl;
-  std::cout << "Door status: " << _doorStatus << std::endl;
-  std::cout << "Catch detect: " << _catchDetect << std::endl;
-  std::cout << "Trap displacement: " << _trapDisplacement << std::endl;
-  std::cout << "Battery status: " << static_cast<int>(_batteryStatus) << std::endl;
-  std::cout << "Unix time: " << _unixTime << std::endl;
+    /**
+     * Prints the decoded payload information.
+     */
+    std::cout << "Payload decoded: " << std::endl;
+    std::cout << "ID: " << _id << std::endl;
+    std::cout << "Version: " << static_cast<int>(_version) << std::endl;
+    std::cout << "Door status: " << _doorStatus << std::endl;
+    std::cout << "Catch detect: " << _catchDetect << std::endl;
+    std::cout << "Trap displacement: " << _trapDisplacement << std::endl;
+    std::cout << "Battery status: " << static_cast<int>(_batteryStatus) << std::endl;
+    std::cout << "Unix time: " << _unixTime << std::endl;
 }
--- payloadCoder/encoder.h
+++ payloadCoder/encoder.h
@@ -31,120 +31,102 @@ const uint8_t SENSOR_PAYLOAD_SIZE = 11; ///< Payload size for sensor
 class payloadEncoder
 {
 private:
-  uint32_t _id;           // Identification number (4 bytes)
-  uint8_t _version;       // Payload version number (1 byte)
-  bool _doorStatus;       // Door status (1 bit)
-  bool _catchDetect;      // Catch detection (1 bit)
-  bool _trapDisplacement; // Trap displacement (1 bit)
-  uint8_t _batteryStatus; // Battery status (1 byte)
-  uint32_t _unixTime;     // Date and time (4 bytes)
+    uint32_t _id;           ///< Identification number (4 bytes)
+    uint8_t _version;       ///< Payload version number (1 byte)
+    bool _doorStatus;       ///< Door status (1 bit)
+    bool _catchDetect;      ///< Catch detection (1 bit)
+    bool _trapDisplacement; ///< Trap displacement (1 bit)
+    uint8_t _batteryStatus; ///< Battery status (1 byte)
+    uint32_t _unixTime;     ///< Date and time (4 bytes)
 
-  uint8_t *_buffer;    ///< buffer containing payload with sensor data
-  uint8_t _bufferSize; ///< Size of payload for housekeeping.
+    uint8_t *_buffer;    ///< buffer containing payload with sensor data
+    uint8_t _bufferSize; ///< Size of payload for housekeeping.
 
-  /**
-   * Adds a uint8_t value to the buffer at the specified index.
-   *
-   * @param idx_in The index in the buffer where the value should be added.
-   * @param value The uint8_t value to be added to the buffer.
-   * @return First free location at which new dat acan be stored in _buffer
-   */
-  unsigned char add_uint8(unsigned char idx_in, const uint8_t value);
+    /// @brief add uint8_t value to the buffer at the specified index.
+    /// @param idx_in The index in the buffer where the value should be added.
+    /// @param value The uint8_t value to be added to the buffer.
+    /// @return First free location at which new dat acan be stored in _buffer
+    unsigned char add_uint8(unsigned char idx_in, const uint8_t value);
 
-  /**
-   * Adds a uint16_t value to the buffer at the specified index.
-   *
-   * @param idx_in The index in the buffer where the value should be added.
-   * @param value The uint16_t value to be added to the buffer.
-   * @return First free location at which new dat acan be stored in _buffer
-   */
-  unsigned char add_uint16(unsigned char idx_in, const uint16_t value);
+    /// @brief  add uint16 to payload
+    /// @param idx_in start location in _buffer
+    /// @param value uint16_t value
+    /// @return First free location at which new dat acan be stored in _buffer
+    unsigned char add_uint16(unsigned char idx_in, const uint16_t value);
 
-  /**
-   * Adds a uint32_t value to the buffer at the specified index.
-   *
-   * @param idx_in The index in the buffer where the value should be added.
-   * @param value The uint32_t value to be added to the buffer.
-   * @return First free location at which new dat acan be stored in _buffer
-   */
-  unsigned char add_uint32(unsigned char idx_in, uint32_t value);
+    /// @brief add uint32 to payload
+    /// @param idx_in start location in _buffer
+    /// @param value uint32_t value
+    /// @return First free location at which new dat acan be stored in _buffer
+    unsigned char add_uint32(unsigned char idx_in, uint32_t value);
 
-  /**
-   * Adds a boolean value to the payload buffer at the specified position.
-   *
-   * This function sets or clears a specific bit in the buffer based on the given boolean value.
-   * If the value is true, the bit at the specified position is set to 1. If the value is false,
-   * the bit at the specified position is cleared to 0.
-   * If the position is 0, the index is incremented so that
-   * the next boolean value is added to the next byte in the buffer.
-   *
-   * @param idx_in The index of the buffer where the boolean value will be added.
-   * @param value The boolean value to be added to the buffer.
-   * @param pos The position of the bit in the buffer where the boolean value will be added.
-   * @return First free location at which new dat acan be stored in _buffer
-   */
-  unsigned char add_bool(unsigned char idx_in, bool value, unsigned int pos);
+    /// @brief add boolean value to the buffer at the specified index.
+    /// @param idx_in The index in the buffer where the value should be added.
+    /// @param value The boolean value to be added to the buffer.
+    /// @param pos The position in the byte where the boolean value should be added.
+    /// @return First free location at which new dat acan be stored in _buffer
+    unsigned char add_bool(unsigned char idx_in, bool value, unsigned int pos);
 
 public:
-  payloadEncoder();  ///< Constructor
-  ~payloadEncoder(); ///< Destructor
+    payloadEncoder();  ///< Constructor
+    ~payloadEncoder(); ///< Destructor
 
-  /// @brief print the payload in binary format
-  /// This function prints the payload in binary format to the console.
-  void printPayloadBinary();
+    /// @brief print the payload in binary format
+    /// This function prints the payload in binary format to the console.
+    void printPayloadBinary();
 
-  /// @brief compose payload
-  /// This function composes the payload by adding all the sensor data and status information to the buffer.
-  void composePayload();
+    /// @brief compose payload
+    /// This function composes the payload by adding all the sensor data and status information to the buffer.
+    void composePayload();
 
-  /// @brief get payload size
-  /// This function returns the size of the payload in bytes.
-  /// @return The size of the payload in bytes.
-  uint8_t getPayloadSize() { return _bufferSize; };
+    /// @brief get payload size
+    /// This function returns the size of the payload in bytes.
+    /// @return The size of the payload in bytes.
+    uint8_t getPayloadSize() { return _bufferSize; };
 
-  /// @brief get payload
-  /// This function returns a pointer to the buffer containing the encoded payload.
-  /// @return A pointer to the buffer containing the encoded payload.
-  uint8_t *getPayload() { return _buffer; };
+    /// @brief get payload
+    /// This function returns a pointer to the buffer containing the encoded payload.
+    /// @return A pointer to the buffer containing the encoded payload.
+    uint8_t *getPayload() { return _buffer; };
 
-  /// @brief set the identification number
-  /// This function sets the identification number in the payload.
-  /// @param id The identification number to be set in the payload.
-  void set_id(const uint32_t id) { _id = static_cast<uint32_t>(id); }
+    /// @brief set the identification number
+    /// This function sets the identification number in the payload.
+    /// @param id The identification number to be set in the payload.
+    void set_id(const uint32_t id) { _id = static_cast<uint32_t>(id); }
 
-  /// @brief set the payload version number
-  /// This function sets the payload version number in the payload.
-  /// @param version The payload version number to be set in the payload.
-  void set_version(const uint8_t version) { _version = static_cast<uint8_t>(version); }
+    /// @brief set the payload version number
+    /// This function sets the payload version number in the payload.
+    /// @param version The payload version number to be set in the payload.
+    void set_version(const uint8_t version) { _version = static_cast<uint8_t>(version); }
 
-  /// @brief set the door status
-  /// This function sets the door status in the payload.
-  /// @param doorStatus The door status to be set in the payload.
-  void set_doorStatus(const bool doorStatus) { _doorStatus = doorStatus; }
+    /// @brief set the door status
+    /// This function sets the door status in the payload.
+    /// @param doorStatus The door status to be set in the payload.
+    void set_doorStatus(const bool doorStatus) { _doorStatus = doorStatus; }
 
-  /// @brief set the catch detection status
-  /// This function sets the catch detection status in the payload.
-  /// @param catchDetect The catch detection status to be set in the payload.
-  void set_catchDetect(const bool catchDetect) { _catchDetect = catchDetect; }
+    /// @brief set the catch detection status
+    /// This function sets the catch detection status in the payload.
+    /// @param catchDetect The catch detection status to be set in the payload.
+    void set_catchDetect(const bool catchDetect) { _catchDetect = catchDetect; }
 
-  /// @brief set the trap displacement status
-  /// This function sets the trap displacement status in the payload.
-  /// @param trapDisplacement The trap displacement status to be set in the payload.
-  void set_trapDisplacement(const bool trapDisplacement) { _trapDisplacement = trapDisplacement; }
+    /// @brief set the trap displacement status
+    /// This function sets the trap displacement status in the payload.
+    /// @param trapDisplacement The trap displacement status to be set in the payload.
+    void set_trapDisplacement(const bool trapDisplacement) { _trapDisplacement = trapDisplacement; }
 
-  /// @brief set the battery status
-  /// This function sets the battery status in the payload.
-  /// @param batteryStatus The battery status to be set in the payload.
-  void set_batteryStatus(const uint8_t batteryStatus) { _batteryStatus = static_cast<uint8_t>(batteryStatus); }
+    /// @brief set the battery status
+    /// This function sets the battery status in the payload.
+    /// @param batteryStatus The battery status to be set in the payload.
+    void set_batteryStatus(const uint8_t batteryStatus) { _batteryStatus = static_cast<uint8_t>(batteryStatus); }
 
-  /// @brief set the date and time
-  /// This function sets the date and time in the payload.
-  /// @param unixTime The date and time to be set in the payload.
-  void set_unixTime(const uint32_t unixTime) { _unixTime = static_cast<uint32_t>(unixTime); }
+    /// @brief set the date and time
+    /// This function sets the date and time in the payload.
+    /// @param unixTime The date and time to be set in the payload.
+    void set_unixTime(const uint32_t unixTime) { _unixTime = static_cast<uint32_t>(unixTime); }
 
-  /// @brief print the encoded payload
-  /// This function prints the encoded payload in a human-readable format to the console.
-  void printPayloadEncoded();
+    /// @brief print the encoded payload
+    /// This function prints the encoded payload in a human-readable format to the console.
+    void printPayloadEncoded();
 };
 
 #endif // ENCODER_H
